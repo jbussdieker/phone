70893fdffe2af5c9e46a40ab20ea6c313fbcb593
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-08 00:57:33 -0700

    Random quote

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,15 @@ class HomeController < ApplicationController
   end
 
   def selection
-    if params[:Digits] == "1"
+    if params[:Digits] == "0"
+      open("http://www.quotesdaddy.com/feed/tagged/Love") do |http|
+        response = http.read
+        result = RSS::Parser.parse(response, false)
+        @quote = result.channel.item.description
+        render :action => "quote.xml.builder", :layout => false
+        return
+      end
+    elsif params[:Digits] == "1"
       @song_url = "https://s3.amazonaws.com/jbussdieker/music/All+Around+You.mp3"
     elsif params[:Digits] == "2"
       @song_url = "https://s3.amazonaws.com/jbussdieker/music/Master+Plan.mp3"
--- app/views/home/call.xml.builder
+++ app/views/home/call.xml.builder
@@ -2,6 +2,6 @@ xml.instruct!
 xml.Response do
     xml.Gather(:action => "selection", :numDigits => 1) do
         xml.Say "Hello my love"
-        xml.Say "Press 1 for All Around You, Press 2 for Master Plan, Press 3 for Wagon Wheel, Press 4 for Flight 180, Press 5 for Only Fools Rush In"
+        xml.Say "Press 0 for Inspiration, Press 1 for All Around You, Press 2 for Master Plan, Press 3 for Wagon Wheel, Press 4 for Flight 180 and Press 5 for Only Fools Rush In"
     end
 end
