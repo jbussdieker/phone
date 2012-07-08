7be609b1e5fddb0a5f3ba0e784576f107bd4149a
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-07 23:11:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phone::Application.config.secret_token = 'b2b06acb90a9428d9e8b140744dae24d0f113903d6533c92d40aa526e31f7110bd604cb4700217af7e39fcc614296f31f6773041fe51450328ff1bfe589a334f'
