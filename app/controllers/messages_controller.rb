d18f1ac6e5fbf9345044b2ff5ac0614b56a8bd25
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-13 00:25:26 -0700

    Newest at top

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,6 @@ class MessagesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @messages = Message.all(:order => 'created_at')
+    @messages = Message.all(:order => 'created_at DESC')
   end
 end
