c4150c82d83a6e9430a23d6008f518edda5988a9
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-16 15:54:03 -0700

    Fix typo

--- app/helpers/calls_helper.rb
+++ app/helpers/calls_helper.rb
@@ -1,8 +1,8 @@
 module CallsHelper
   def render_call_status(call)
     classname = "label"
-    classname << " label-warning" if call.CallStatus != "ringing"
-    classname << " label-success" if call.CallStatus != "in-progress"
+    classname << " label-warning" if call.CallStatus == "ringing"
+    classname << " label-success" if call.CallStatus == "in-progress"
 
     content_tag(:span, call.CallStatus, :class => classname)
   end
