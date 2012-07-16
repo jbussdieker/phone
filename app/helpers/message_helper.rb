006078d7f2a6da73520ad86d8c1748c7374f18ed
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-16 13:22:11 -0700

    Use caller info to display name

--- app/helpers/message_helper.rb
+++ app/helpers/message_helper.rb
@@ -2,4 +2,10 @@ module MessageHelper
     def message_badge(message)
         "New" if message.new
     end
+
+    def caller_name(message)
+        n = Number.where("number = ?", message.number).first
+        message.number
+        n.name if n
+    end
 end
